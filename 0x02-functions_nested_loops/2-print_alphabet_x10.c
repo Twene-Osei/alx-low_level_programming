4776d39130b12dcd5f49da233f3298cd78a6a046
“Twene-Osei” <sikapa447@gmail.com>
2022-10-19 21:03:51 -0700

    men

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10-prints all alphabets in lowercase 10 times.
+ * Return: On success 1.
+ */
+
+void print_alphabet_x10(void)
+{
+	char j,z;
+
+
+	for (z=0; z<10; z++)
+	{
+
+		for (j='a', j <= 'z'; j++)
+
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+
+
+	}
+
+}
